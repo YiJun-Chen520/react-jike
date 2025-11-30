7a2ad30362d4f3c52a1a2819adac90605835bb74
陈怡俊 <14838102+chen-yijun0408@user.noreply.gitee.com>
2025-11-30 20:18:30 +0800

    处理token失效功能实现

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,7 +1,7 @@
 // axios的封装处理
 
 import axios from 'axios'
-import { getToken } from '@/utils'
+import { getToken, removeToken } from '@/utils/token'
 
 // 1.根域名的配置
 // 2.超时时间
@@ -21,6 +21,7 @@ request.interceptors.request.use((config) => {
   }
   return config
 }, (error) => {
+
   return Promise.reject(error)
 })
 
@@ -32,6 +33,16 @@ request.interceptors.response.use((response) => {
 }, (error) => {
   // 超出 2xx 范围的状态码都会触发该函数。
   // 对响应错误做点什么
+  console.dir(error)
+  if (error.response.status === 401) {
+    removeToken()
+    // 延迟导入router，避免循环依赖
+    import('@/router').then(module => {
+      module.default.navigate('/login')
+      window.location.reload()
+    })
+    window.location.reload()
+  }
   return Promise.reject(error)
 })
 
