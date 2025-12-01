3bc0ad8f2d6d75986daf27719fd15b05f60dfc01
陈怡俊 <14838102+chen-yijun0408@user.noreply.gitee.com>
2025-12-01 22:30:23 +0800

    富文本编辑器

--- src/index.js
+++ src/index.js
@@ -9,10 +9,8 @@ import router from './router';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <RouterProvider router={router} />
-    </Provider>
-  </React.StrictMode>
+  <Provider store={store}>
+    <RouterProvider router={router} />
+  </Provider>
 );
 
